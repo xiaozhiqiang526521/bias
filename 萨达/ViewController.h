0cc5fa6446dd01a6036d427064a33d78777a566c
xiaozhiqiang526521 <1107178704@qq.com>
2016-07-18 20:27:35 +0800

    Initial Commit

---
+++ 萨达/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  萨达
+//
+//  Created by garday on 16/7/18.
+//  Copyright © 2016年 garday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 萨达/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  萨达
+//
+//  Created by garday on 16/7/18.
+//  Copyright © 2016年 garday. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
